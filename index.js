1d766b9dfe029a3b7a2df3317ce51c4ce42b2e04
halfway-there1 <90105000+halfway-there1@users.noreply.github.com>
2023-08-11 20:39:09 +0530

    Reorgainze the code in index.js

--- index.js
+++ index.js
@@ -1,50 +1,57 @@
-let bookTitle = document.querySelector('#book-title');
-let author = document.querySelector('#author');
-let pageCount = document.querySelector('#page-count');
-let readStatus = document.querySelector('#read-status');
+// form elements
+const bookTitle = document.querySelector('#book-title');
+const author = document.querySelector('#author');
+const pageCount = document.querySelector('#page-count');
+const readStatus = document.querySelector('#read-status');
+const addBookBtn = document.querySelector('#add-book-btn');
 
-let addBookBtn = document.querySelector('#add-book-btn');
+const bookContainer = document.querySelector('.book-container');
 
-let bookContainer = document.querySelector('.book-container');
+// actual code begins
 
-addBookBtn.addEventListener('click', (event) => {
-  event.preventDefault();
-  addBookToLibrary();
-});
-
-function createBookElement(book) {
-  let bookEle = document.createElement('div');
-  bookEle.classList.add('book');
-
-  let bookInfoEle = document.createElement('p');
-  bookInfoEle.textContent = book.info();
-
-  bookEle.append(bookInfoEle);
-  bookContainer.append(bookEle);
-}
-
-function addBookToLibrary() {
-  library.push(
-    new Book(bookTitle.value, author.value, pageCount.value, readStatus.checked)
-  );
-  createBookElement(library[library.length - 1]);
-}
-
-let library = [];
+const library = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-
-  this.info = function () {
-    let info = `${this.title} by ${this.author}, ${this.pages} pages`;
-    info += read ? ', read' : ', not read yet';
-    return info;
-  };
 }
 
-let b1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
+Book.prototype.info = function () {
+  let info = `${this.title} by ${this.author}, ${this.pages} pages`;
+  info += this.read ? ', read' : ', not read yet';
 
+  return info;
+};
+
+let b1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
 console.log(b1.info());
+
+addBookBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  addBookToLibrary();
+});
+
+function addBookToLibrary() {
+  library.push(
+    new Book(bookTitle.value, author.value, pageCount.value, readStatus.checked)
+  );
+
+  const bookEle = createBookElement(library[library.length - 1]);
+  bookContainer.append(bookEle);
+}
+
+// creates a book-element on the dom using the
+// book parameter
+function createBookElement(book) {
+  const bookEle = document.createElement('div');
+  bookEle.classList.add('book');
+
+  const bookInfoEle = document.createElement('p');
+  bookInfoEle.textContent = book.info();
+
+  bookEle.append(bookInfoEle);
+
+  return bookEle;
+}
